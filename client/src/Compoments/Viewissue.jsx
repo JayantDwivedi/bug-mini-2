88eac6cba6d7b3716b11c7be00b6340ff85bda91
Jayant Dwivedi <jayantdwivedi19@gmail.com>
2021-04-12 13:09:34 +0530

    card view display

--- client/src/Compoments/Viewissue.jsx
+++ client/src/Compoments/Viewissue.jsx
@@ -13,10 +13,10 @@ const Viewissue = () => {
 
   const getData = () => {
     axios.get("http://localhost:5000/viewissue").then((result) => {
-      // console.log(result.data);
       setData({ info: result.data });
-      console.log("use effect called");
-      console.log(data.info);
+      // console.log(result.data);
+      // console.log("use effect called");
+      // console.log(data.info);
     });
   };
 
@@ -25,58 +25,42 @@ const Viewissue = () => {
     getData();
   }, []);
 
-  // console.log(data.info[1].projectname);
-
-  // console.log(result.data);
-
   const newinfo = data.info.map((value) => {
     return value;
   });
-  console.log(newinfo);
 
-  return (
-    // <div className="view-area">
-    //   {data.info.map((value) => {
-    //     <Card style={{ width: "18rem" }}>
-    //       <Card.Body>
-    //         <Card.Title> Project Name : {value.projectname} </Card.Title>
-    //         <Card.Subtitle className="mb-2 text-muted">
-    //           Issue Name : {value.issuename}
-    //         </Card.Subtitle>
-    //         <Card.Subtitle className="mb-2 text-muted">
-    //           Priority: {value.priority}
-    //         </Card.Subtitle>
-    //         <Card.Text>Desc:{value.desc}</Card.Text>
-    //         <Card.Link href="#" className="btn btn-danger">
-    //           Card Link
-    //         </Card.Link>
-    //         <Card.Link href="#" className="btn btn-warning">
-    //           Another Link
-    //         </Card.Link>
-    //       </Card.Body>
-    //     </Card>;
-    //   })}
-    // </div>
+  // const issuename = newinfo.map((value) => {
+  //   return value.issuename;
+  // });
 
-    <div className="view-area">
-      <Card style={{ width: "18rem" }}>
-        <Card.Body className="high">
-          <Card.Title> Project Name : </Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">
-            Issue Name :
-          </Card.Subtitle>
-          <Card.Subtitle className="mb-2 text-muted">Priority:</Card.Subtitle>
-          <Card.Text>Desc:</Card.Text>
-          <Card.Link href="#" className="btn btn-danger">
-            Card Link
-          </Card.Link>
-          <Card.Link href="#" className="btn btn-warning">
-            Another Link
-          </Card.Link>
-        </Card.Body>
-      </Card>
-    </div>
-  );
+  // console.log(issuename);
+
+  const renderData = (value, index) => {
+    return (
+      <div key={index} className="row">
+        <Card style={{ width: "18rem" }} className={`mycard ${value.priority}`}>
+          <Card.Body>
+            <Card.Title> Project Name :{value.projectname} </Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">
+              Issue Name : {value.issuename}
+            </Card.Subtitle>
+            <Card.Subtitle className="mb-2 text-muted">
+              Priority: {value.priority}
+            </Card.Subtitle>
+            <Card.Text>Desc: {value.desc}</Card.Text>
+            <Card.Link href="#" className="btn btn-danger">
+              Card Link
+            </Card.Link>
+            <Card.Link href="#" className="btn btn-warning">
+              Another Link
+            </Card.Link>
+          </Card.Body>
+        </Card>
+      </div>
+    );
+  };
+
+  return <div className="view-area">{newinfo.map(renderData)}</div>;
 };
 
 export default Viewissue;
